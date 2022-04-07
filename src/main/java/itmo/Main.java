2a890b4298822706fa3ce6650d27d1cbf844ed8e
plok1ju <qwertyplokiju1509@gmail.com>
2022-04-07 22:12:29 +0300

    fix: console enter

--- src/main/java/itmo/Main.java
+++ src/main/java/itmo/Main.java
@@ -1,10 +1,37 @@
 package itmo;
 
+import itmo.collection.HashTableCollection;
+import itmo.commands.Command;
+import itmo.io.ConsoleScan;
+import itmo.io.Scannable;
+import itmo.manager.CommandsManager;
+import itmo.manager.file.ReaderXml;
+import itmo.model.Dragon;
+
 public class Main {
+    private static HashTableCollection<Integer, Dragon> collection = new HashTableCollection<>();
     public static void main(String[] args) throws Exception {
+        ReaderXml readerXml = new ReaderXml();
+        try{
+            collection = readerXml.returnCollect();
+        }
+        catch (Exception e){
 
+        }
+        Scannable scannable = new ConsoleScan();
+        CommandsManager commandsManager = new CommandsManager(collection);
+        getCommand(commandsManager, scannable);
 
+    }
 
+    private static void getCommand(CommandsManager commandsManager, Scannable scannable){
+        try{
+            Command command = commandsManager.getCommand(scannable.scanString(), scannable,true);
+
+        } catch (Exception e) {
+            e.getMessage();
+        }
+        getCommand(commandsManager, scannable);
     }
 }
 
